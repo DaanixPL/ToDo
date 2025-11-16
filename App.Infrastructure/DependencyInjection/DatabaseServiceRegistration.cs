081d3b8910ae229ca53521dc37d7e56c929d04a0
vDanix <dlitwin04@outlook.com>
2025-11-16 01:35:53 +0100

    added docerfiles

--- App.Infrastructure/DependencyInjection/DatabaseServiceRegistration.cs
+++ App.Infrastructure/DependencyInjection/DatabaseServiceRegistration.cs
@@ -7,7 +7,7 @@ namespace ToDo.Infrastructure.DependencyInjection
 {
     public static class DatabaseServiceRegistration
     {
-        public static IServiceCollection AddDatabaseServices(this IServiceCollection services, IConfiguration configuration)
+        public static IServiceCollection AddDatabaseServices(this IServiceCollection services, IConfiguration configuration, string)
         {
             services.AddDbContext<AppDbContext>(options =>
             options.UseMySql(configuration.GetConnectionString("DefaultConnection"),
